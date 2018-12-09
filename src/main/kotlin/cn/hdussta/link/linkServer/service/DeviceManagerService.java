03a0162e1e3c1f76e0dd358c825a3ef96f6e3d82
Ginkre <867653608@qq.com>
2018-12-09 16:38:16 +0800

    init

--- src/main/kotlin/cn/hdussta/link/linkServer/service/DeviceManagerService.java
+++ src/main/kotlin/cn/hdussta/link/linkServer/service/DeviceManagerService.java
@@ -6,7 +6,6 @@ import io.vertx.codegen.annotations.ProxyGen;
 import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
-import io.vertx.core.json.JsonObject;
 
 @ProxyGen
 @VertxGen
